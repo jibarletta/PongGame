1d7b259399b671cd0a9a31f189f542ce7fcd5384
Juan Ignacio <jbarletta@sociales.uba.ar>
2021-03-15 17:07:38 -0300

    Pong Game día 22 del curso de Angela YU de python.

--- ball.py
+++ ball.py
@@ -5,7 +5,7 @@ MOVE_DISTANCE = 20
 class Ball(Turtle):
 
     def __init__(self):
-        super(Ball, self).__init__()
+        super().__init__()
         self.shape('circle')
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.color('white')
@@ -13,12 +13,21 @@ class Ball(Turtle):
         self.speed(1)
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def refresh(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
-
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
+score = Scoreboard()
+
 
 r_player = Paddle((350, 0))
 l_player = Paddle((-350, 0))
 pelota = Ball()
 
 screen.listen()
-screen.onkey(fun=r_player.up, key='Up')
-screen.onkey(fun=r_player.down, key='Down')
-screen.onkey(fun=l_player.up, key='w')
-screen.onkey(fun=l_player.down, key='s')
+screen.onkeypress(fun=r_player.up, key='Up')
+screen.onkeypress(fun=r_player.down, key='Down')
+screen.onkeypress(fun=l_player.up, key='w')
+screen.onkeypress(fun=l_player.down, key='s')
 
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(pelota.move_speed)
     screen.update()
     pelota.move()
 
     # Detect Collision
     if pelota.ycor() > 280 or pelota.ycor() < -280:
-        pelota.bounce()
+        pelota.bounce_y()
+
+    # Detect collision with r_paddle
+    if pelota.distance(r_player) < 50 and pelota.xcor() > 320 or pelota.distance(
+            l_player) < 50 and pelota.xcor() < -320:
+        pelota.bounce_x()
+
+    # Detect when ball misses player
+    if pelota.xcor() > 400:
+        pelota.refresh()
+        score.l_point()
+
+    if pelota.xcor() < -400:
+        pelota.refresh()
+        score.r_point()
 
 screen.exitonclick()
